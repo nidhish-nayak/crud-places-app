3576c7dc78654e1e02e8d8ae3fb0180f9caa37fb
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-04-25 22:58:04 +0530

    Added more styles tp userList

--- client/src/user/pages/User.pages.jsx
+++ client/src/user/pages/User.pages.jsx
@@ -1,12 +1,27 @@
 import UserList from "../components/UserList/UserList.component";
 
 const User = () => {
-    const USERS = [{
-        id: 1,
-        name: "John",
-        image: "https://img.icons8.com/external-avatar-andi-nur-abdillah/64/null/external-avatar-business-avatar-circle-avatar-andi-nur-abdillah-14.png",
-        places: 3
-    }];
+    const USERS = [
+        {
+            id: 1,
+            name: "John Martin",
+            image: "https://img.icons8.com/external-avatar-andi-nur-abdillah/64/null/external-avatar-business-avatar-circle-avatar-andi-nur-abdillah-14.png",
+            places: 3,
+        },
+        {
+            id: 2,
+            name: "Nidhish D Nayak",
+            image: "https://img.icons8.com/external-avatar-andi-nur-abdillah/64/null/external-avatar-business-avatar-circle-avatar-andi-nur-abdillah-14.png",
+            places: 7,
+        },
+        {
+            id: 3,
+            name: "Kevin Ashton",
+            image: "https://img.icons8.com/external-avatar-andi-nur-abdillah/64/null/external-avatar-business-avatar-circle-avatar-andi-nur-abdillah-14.png",
+            places: 5,
+        },
+
+    ];
 
     return (
         <UserList items={USERS} />
