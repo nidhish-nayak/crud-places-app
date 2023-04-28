a2402da6761bbd746818f046e684feb657d89a48
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-04-28 10:16:03 +0530

    Added styles-minor

--- client/src/user/components/UserItem/UserItem.component.jsx
+++ client/src/user/components/UserItem/UserItem.component.jsx
@@ -8,8 +8,8 @@ const UserItem = props => {
                     <img src={props.image} alt="alt" />
                 </div>
                 <div className="user-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}</h3>
+                    <h3>{props.name}</h3>
+                    <h4>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}</h4>
                 </div>
             </div>
         </li>
