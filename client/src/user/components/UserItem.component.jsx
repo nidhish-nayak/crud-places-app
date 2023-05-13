3b458428a3305061f6ca603d6d70bba9b67e46c4
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-05-13 20:21:36 +0530

    Converted list items to div - able to reuse the Card when needed in future

--- client/src/user/components/UserItem.component.jsx
+++ client/src/user/components/UserItem.component.jsx
@@ -3,7 +3,7 @@ import Avatar from "../../shared/components/Avatar.component";
 
 const UserItem = props => {
     return (
-        <li className="w-64 m-2 text-white transition-all ease-linear scale-95 rounded-md cursor-pointer duration-250 drop-shadow-sm bg-gradient-to-r from-gray-900 to-neutral-700 sm:w-72 hover:drop-shadow-2xl hover:scale-100">
+        <div className="w-64 m-2 text-white transition-all ease-linear scale-95 rounded-md cursor-pointer duration-250 drop-shadow-sm bg-gradient-to-r from-gray-900 to-neutral-700 sm:w-72 hover:drop-shadow-2xl hover:scale-100">
             <Link to={`/${props.id}/places`}>
                 <div className="flex items-center justify-start p-4">
                     <Avatar myAvatar={props.image} />
@@ -13,7 +13,7 @@ const UserItem = props => {
                     </div>
                 </div>
             </Link>
-        </li>
+        </div>
     )
 }
 
