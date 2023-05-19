7ad5cdb65660bdbb6b270782b07b53b77abe5845
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-05-19 18:01:47 +0530

    Fixed issue where text did not appear in the modal

--- client/src/places/components/PlaceItem.component.jsx
+++ client/src/places/components/PlaceItem.component.jsx
@@ -29,7 +29,7 @@ const PlaceItem = (props) => {
             </div>
             <div className="flex m-4">
                 <button onClick={handleViewClick} className="px-2 py-2 m-1 text-xs font-normal text-black transition-all duration-200 ease-linear bg-white rounded-md cursor-pointer sm:px-3 sm:font-semibold sm:text-sm hover:bg-teal-300 hover:text-neutral-900">VIEW ON MAP</button>
-                <Modal isOpen={isOpen} onClose={handleCloseModal} title={title} description={description} />
+                <Modal isOpen={isOpen} onClose={handleCloseModal} title={props.title} description={props.address} />
                 <button className="px-2 py-2 m-1 text-xs font-normal transition-all duration-200 ease-linear bg-green-600 rounded-md cursor-pointer sm:px-3 sm:font-semibold sm:text-sm hover:bg-teal-300 hover:text-neutral-900">EDIT</button>
                 <button className="px-2 py-2 m-1 text-xs font-normal transition-all duration-200 ease-linear bg-red-600 rounded-md cursor-pointer sm:px-3 sm:font-semibold sm:text-sm hover:bg-teal-300 hover:text-neutral-900">DELETE</button>
             </div>
--- client/src/shared/components/Modal.component.jsx
+++ client/src/shared/components/Modal.component.jsx
@@ -6,9 +6,10 @@ const Modal = ({ isOpen, onClose, title, description }) => {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center m-2 sm:m-0">
             <div className="fixed inset-0 bg-gray-900 opacity-50" onClick={onClose}></div>
-            <div className="z-10 p-6 bg-white rounded-lg shadow-lg w-96">
+            <div className="z-10 p-6 text-black bg-white rounded-lg shadow-lg w-96">
                 <h2 className="mb-4 text-xl font-bold">{title}</h2>
                 <p className="mb-6">{description}</p>
+                <div className="mb-6">PlaceHolder for the map!</div>
                 <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600" onClick={onClose}>
                     Close
                 </button>
