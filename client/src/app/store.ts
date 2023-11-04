1b12b9315df8f0363c8e63fc7c3ad635573f95af
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-11-05 01:38:50 +0530

    Added inputReducer in store.ts

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import inputReducer from "../features/input/inputSlice";
 import modalReducer from "../features/modal/modalSlice";
 
 export const store = configureStore({
 	reducer: {
 		modal: modalReducer,
+		input: inputReducer,
 	},
 });
 
