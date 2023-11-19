a2626076ef86af38670f4a837ace4a2a28ffcbf5
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-11-19 22:43:16 +0530

    added a message for page update fail

--- client/src/components/update-place/update-place.tsx
+++ client/src/components/update-place/update-place.tsx
@@ -1,8 +1,34 @@
 import { useParams } from "react-router-dom";
 
+import { DUMMY_PLACES } from "../../routes/user-place/data";
+import { VALIDATOR_REQUIRE } from "../../utils/validators";
+import Input from "../input/input";
+
 const UpdatePlace = () => {
 	const placeId = useParams().placeId;
-	return <div>Update Place: {placeId}</div>;
+	const identifiedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
+
+	if (!identifiedPlace) {
+		return (
+			<div className="flex justify-center text-center">
+				<h2 className="text-xl">Could not find the place!</h2>
+			</div>
+		);
+	}
+
+	return (
+		<form>
+			<Input
+				id="title"
+				element="input"
+				type="text"
+				label="Title"
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText="Please enter a valid title"
+				value="{identifiedPlace.title}"
+			/>
+		</form>
+	);
 };
 
 export default UpdatePlace;
